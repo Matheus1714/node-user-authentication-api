0948fbf4b87b842dee650757545e6554331e8709
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-08 22:29:36 -0300

    config settings routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello")
+import express from 'express'
+import statusRoute from './routes/status.route'
+import userRoute from './routes/users.route'
+
+require('dotenv').config()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(userRoute)
+app.use(statusRoute)
+
+app.listen(process.env.PORT, () => console.log('Listen express'))
