040b6737c46a1b3d23e3f2c993b28c2571417d06
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-09 00:35:33 -0300

    config express with error

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,17 @@ import statusRoute from './routes/status.route'
 import userRoute from './routes/users.route'
 import dotenv from 'dotenv'
 import db from './db'
-
-dotenv.config()
+import errorHandler from './middlewares/error-handler.middleware'
 
 const app = express()
+dotenv.config()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(userRoute)
 app.use(statusRoute)
+
+app.use(errorHandler)
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
